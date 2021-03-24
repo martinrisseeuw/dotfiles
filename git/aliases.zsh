41ecbe737ab39f91dae071bd007dada8f7225533
Martin Risseeuw <hello@martinr.nl>
2021-03-24 11:10:12 +0100

    Added gri alias
    --wip-- [skip ci]

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,3 +10,5 @@ alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
+alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify --no-gpg-sign -m "--wip-- [skip ci]"'
+alias gri='git rebase -i'
